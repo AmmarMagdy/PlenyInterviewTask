9db0b158c651598e6a1c1ac9139c2df1dd04a621
Ammar Magdy <ammar.m.badawi@gmail.com>
2023-09-16 23:56:23 +0300

    add post header

--- PlenyInterviewTask/UI/Posts/View/PostView.swift
+++ PlenyInterviewTask/UI/Posts/View/PostView.swift
@@ -11,24 +11,73 @@ struct PostView: View {
     
     @ObservedObject var viewModel: PostsViewModel
     
+    @State var text: String = ""
+    @State var showTextField: Bool = false
+    
     var body: some View {
-        ScrollView {
-            LazyVStack {
-                ForEach(0..<viewModel.items.count, id: \.self) { index in
-                    PostCell(post: viewModel.post(at: index))
-                        .onAppear() {
-                            viewModel.loadMoreContent(post: viewModel.post(at: index))
-                        }
-                    Divider()
+        VStack {
+            headerView
+            Divider().padding(.bottom, 8)
+            ScrollView {
+                LazyVStack {
+                    ForEach(0..<viewModel.items.count, id: \.self) { index in
+                        PostCell(post: viewModel.post(at: index))
+                            .onAppear() {
+                                viewModel.loadMoreContent(post: viewModel.post(at: index))
+                            }
+                        Divider()
+                    }
                 }
-              
+                if viewModel.showProgressView {
+                    ProgressView()
+                }
             }
-            if viewModel.showProgressView {
-                ProgressView()
-            }
-        }
-        .onAppear() {
+        }.onAppear() {
             viewModel.getPosts()
+        }
+    }
+    
+    var headerView: some View {
+        ZStack {
+            if showTextField {
+                HStack {
+                    Image("search")
+                        .resizable()
+                        .frame(width: 16, height: 16)
+                        .padding([.leading, .vertical])
+                    TextField("ammar", text: $text)
+                    Button {
+                        withAnimation(.easeInOut(duration: 0.2)) {
+                            showTextField.toggle()
+                        }
+                    } label: {
+                        Image("close")
+                            .resizable()
+                            .frame(width: 24, height: 24)
+                            .padding(8)
+                    }
+                }.frame(height: 40)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 8)
+                        .stroke(Color.borderColor(), lineWidth: 1)
+                ).frame(height: 40).padding(.horizontal)
+            } else {
+                HStack {
+                    Image("logo")
+                    Spacer()
+                    Button {
+                        withAnimation(.easeOut(duration: 0)) {
+                            showTextField.toggle()
+                        }
+                    } label: {
+                        Image("search")
+                            .resizable()
+                            .frame(width: 25, height: 25)
+                            .padding(8)
+                    }
+                }.padding(.horizontal)
+                .frame(height: 40)
+            }
         }
     }
 }
